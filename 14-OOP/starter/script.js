1463452a9d680d96eacf8358185ce6813dfa27c6
Amr Allam <115567652+Amr-Allam@users.noreply.github.com>
2025-04-07 19:01:03 +0200

    update

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,3 +1,10 @@
 'use strict';
 
-const Person = function (firstName, birthYear) {};
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
