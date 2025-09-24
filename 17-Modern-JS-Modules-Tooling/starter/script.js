4cbe73dc231a3060efd5fca4ab7cca459cfb1958
Amr Allam <115567652+Amr-Allam@users.noreply.github.com>
2025-09-24 23:26:46 +0300

    Finish modules section

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -5,11 +5,66 @@ import add from './shoppingCart.js'; // importing the default
 // we usually never Mix default and named exports (it's recommended to use one default export per module)
 // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 
-console.log('Importing module');
+// console.log('Importing module');
 
 add('pizza', 10);
 ShoppingCart.addToCart('bread', 5);
 
-console.log(ShoppingCart.totalPrice, ShoppingCart.tq);
+// console.log(ShoppingCart.totalPrice, ShoppingCart.tq);
 
-console.log(ShoppingCart.cart); // cart here is connected to the value we exported not just a copy (they are the same object / point to the same place in memory)
+// console.log(ShoppingCart.cart); // cart here is connected to the value we exported not just a copy (they are the same object / point to the same place in memory)
+
+/////////////////////////////////////////////////////////////////////////
+// Top Level await (now blocks the scripts)
+// const rest = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await rest.json();
+// console.log(data);
+// console.log('something');
+
+const getLastPost = async function () {
+  const rest = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await rest.json();
+  // console.log(data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+// console.log(lastPost);
+
+// Not very clean
+// lastPost.then(last => console.log(last));
+
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+
+///////////////////////////////////////////////
+// Importing libraries using npm
+// import cloneDeep from './node_modules/lodash-es/cloneDeep.js'; (we don't do this)
+import { cloneDeep } from 'lodash-es'; // because of parcel
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+
+console.log(stateClone);
+console.log(stateDeepClone);
+
+// To not let the page reload when i change this part (maintain the state of the page)
+if (module.hot) {
+  module.hot.accept();
+}
+
+// For polyfilling (add support for modern js features for old browsers)
+import 'core-js/stable';
+// import 'core-js/stable/array/find'; (To add a certain method only if we want)
+
+// Polyfilling async functions
+import 'regenerator-runtime/runtime';
--- 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/starter/shoppingCart.js
@@ -1,11 +1,11 @@
-console.log("Exporting module");
+// console.log("Exporting module");
 
 const shippingCost = 10;
 export const cart = [];
 
 export const addToCart = function (product, quantity) {
   cart.push({ product, quantity });
-  console.log(`${quantity} ${product} added to cart`);
+  // console.log(`${quantity} ${product} added to cart`);
 };
 
 const totalPrice = 237;
@@ -16,5 +16,9 @@ export { totalPrice, totalQuantity as tq };
 // when we want to export 1 value only per module
 export default function (product, quantity) {
   cart.push({ product, quantity });
-  console.log(`${quantity} ${product} added to cart`);
+  // console.log(`${quantity} ${product} added to cart`);
 }
+
+// console.log('Start fetching users');
+// await fetch('https://jsonplaceholder.typicode.com/users');
+// console.log('Finish fetching users');
